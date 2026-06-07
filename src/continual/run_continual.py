43915fa8dbbcd59595d7600f53341652cab84778
Navneet Sinha <sinhanavneet14@gmail.com>
2026-06-07 15:43:58 +0100

    Use task-aware dataset constructor so LwF/Replay get task labels

--- src/continual/run_continual.py
+++ src/continual/run_continual.py
@@ -29,7 +29,9 @@ from src.models.model import build_model
 
 # --- Avalanche 0.6 imports ---
 from avalanche.benchmarks import benchmark_from_datasets
-from avalanche.benchmarks.utils import as_classification_dataset
+from avalanche.benchmarks.utils import (
+    _make_taskaware_classification_dataset as make_cl_dataset,
+)
 
 from avalanche.training.supervised import Naive, Cumulative, EWC, LwF, Replay
 from avalanche.training.plugins import EvaluationPlugin
@@ -42,8 +44,8 @@ def build_benchmark():
     """One experience per product (domain-incremental)."""
     train_sets, test_sets = [], []
     for product in config.PRODUCT_ORDER:
-        train_sets.append(as_classification_dataset(load_imagefolder(product, "train"), task_labels=0))
-        test_sets.append(as_classification_dataset(load_imagefolder(product, "test"), task_labels=0))
+        train_sets.append(make_cl_dataset(load_imagefolder(product, "train"), task_labels=0))
+        test_sets.append(make_cl_dataset(load_imagefolder(product, "test"), task_labels=0))
     return benchmark_from_datasets(train=train_sets, test=test_sets)
 
 
